14b6c9656f069ce168ad78b9ffa2b714a4adf969
samarijo <sabrinamariano1994@hotmail.com>
2022-10-10 17:50:58 -0300

    conceito de SPA aplicado no print dos cards

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
-import printCards from "./components/App.js";
+import cards from "./pages/cards/index.js";
 
-document.getElementById("print-cards").appendChild(printCards());
+const main = document.querySelector("#root");
+
+window.addEventListener("load",() => {
+    main.appendChild(cards());
+} );
+
+/*document.getElementById("root").appendChild(printCards());*/
