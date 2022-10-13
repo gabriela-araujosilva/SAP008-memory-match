c6c2d856c8b043a4cb7760180ee43948e7f6f28f
samarijo <sabrinamariano1994@hotmail.com>
2022-10-13 13:39:19 -0300

    estilização inicial da página de cards

--- src/pages/cards/index.js
+++ src/pages/cards/index.js
@@ -1,33 +1,32 @@
-
 import data from "./../../data/webdev/webdev.js";
 
 const dataDev = data.items;
 
 const btnPlainAgain = () => {
-  const btnPrint = document.createElement("button");
+  const btnPrint = document.createElement("div");
   btnPrint.innerHTML = `
-   <button type="button" class="btn">Jogar Novamente</button>
+   <button type="button" class="botão">Jogar Novamente</button>
   `;
-   return btnPrint;
-
+  return btnPrint;
 };
 
 const printCards = () => {
-  const container = document.createElement("div");
-  let allCards = "";
-  dataDev.map((personagem) => {
-    allCards+=`
-    <div class="quadro-card">
-    <div>
-        <img class="quadro-imagem" src="${personagem.image}">
-    </div>
-    </div> 
-  `;
-  });
+    const container = document.createElement("div");
+    let allCards = "";
+    dataDev.map((personagem) => {
+      allCards+=`
+      <div class="quadro-card">
+      <div>
+          <img class="quadro-imagem" src="${personagem.image}" id="${personagem.id}">
+          
+      </div>
+      </div> 
+    `;
+    });
 
-  container.innerHTML = allCards + allCards;
+    container.innerHTML = allCards + allCards;
 
-  return container;
+    return container;
 };
 
 export {printCards, btnPlainAgain};
