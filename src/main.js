8bfa0a963861f06deb28785bc06dd66a73c86584
samarijo <sabrinamariano1994@hotmail.com>
2022-10-14 14:53:38 -0300

    tentativa de printar o nome do jogador no campo bem-vinde (nao funcionando)

--- src/main.js
+++ src/main.js
@@ -7,19 +7,22 @@ window.addEventListener("load",() => {
     main.appendChild(printCards());
 });
 
+const printNameGamer = document.querySelector("#print-bem-vinde");
+const input = document.querySelector("#input-redefinir-jogador");
+console.log(input.value);
+printNameGamer.innerHTML = `Bem vinde: ${input.value}`;
+
 const shuffle = (array) => {
-    let currentIndex = array.length,  randomIndex;
-    while (currentIndex != 0) {
+  let currentIndex = array.length,  randomIndex;
+   while (currentIndex != 0) {
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex--;
       [array[currentIndex], array[randomIndex]] = [
         array[randomIndex], array[currentIndex]];
     }
   
-    return array;
-  };
-
-  console.log(shuffle(dataDev));
+  return array;
+};
   
-  /*const btnShuflle = document.querySelector("#botão-shuffle");
-  btnShuflle.addEventListener("click", shuffle(dataDev));*/
+  const btnShuflle = document.querySelector("#botão-shuffle");
+  btnShuflle.addEventListener("click", shuffle(dataDev));
