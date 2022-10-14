a91a8ff05cb880f90c39dfaf9dc0a146d034ef13
samarijo <sabrinamariano1994@hotmail.com>
2022-10-14 18:19:40 -0300

    inclusao do callback no addeventliestener

--- src/main.js
+++ src/main.js
@@ -7,19 +7,5 @@ window.addEventListener("load",() => {
     main.appendChild(printCards());
 });
 
-const shuffle = (array) => {
-    let currentIndex = array.length,  randomIndex;
-    while (currentIndex != 0) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
-    }
   
-    return array;
-  };
-
-  console.log(shuffle(dataDev));
   
-  /*const btnShuflle = document.querySelector("#botão-shuffle");
-  btnShuflle.addEventListener("click", shuffle(dataDev));*/
--- src/pages/cards/index.js
+++ src/pages/cards/index.js
@@ -3,36 +3,37 @@ import data from "./../../data/webdev/webdev.js";
 const dataDev = data.items;
 
 const divElements = () => {
-    const printElements = document.createElement("div");
-    printElements.innerHTML = `
+  const printElements = document.createElement("div");
+  printElements.innerHTML = `
    <div class="elementos-container">
-    <button type="button" class="botão" id="botão-shuffle">Jogar Novamente</button>
-     <form action="/pagina-processa-dados-do-form" method="post">
-        <div>
+    <button type="button" class="btn" id="btn-shuffle">Jogar Novamente</button>
+     <form>
+        
           <label class="text-redefinir-jogador">Redefinir jogador</label>
           <input type="text" id="input-redefinir-jogador"/>
-        </div>
+    
     </form>
     </div>  
     `;
-  return printElements;
-}; 
+  
 
+  return printElements;
+};
 
 const printCards = () => {
-    const container = document.createElement("div");
-    let allCards = "";
-    dataDev.map((personagem) => {
-        allCards+=`
+  const container = document.createElement("div");
+  let allCards = "";
+  dataDev.map((personagem) => {
+    allCards += `
         <div class="quadro-card">
         <div><img class="quadro-imagem" src="${personagem.image}" id="${personagem.id}">
         </div>
         </div>
       `;
-    });
+  });
 
   container.innerHTML = allCards + allCards;
   return container;
 };
 
-export {printCards, divElements, dataDev};
+export { printCards, divElements, dataDev };
