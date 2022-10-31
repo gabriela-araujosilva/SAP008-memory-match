24e6bd95a98ec1f3b419e03442ee23e959a5d710
samarijo <sabrinamariano1994@hotmail.com>
2022-10-31 04:58:00 -0300

    redme parcialmente pronto (falta os testes unitarios)

--- src/pages/cards/cards.spec.js
+++ src/pages/cards/cards.spec.js
@@ -1,4 +1,4 @@
-import {divElements} from "./index.js";
+import {divElements, printCards} from "./index.js";
 
 describe("divElements", () => {
   it("should render without crashing", () => {
@@ -6,3 +6,14 @@ describe("divElements", () => {
     expect(el instanceof HTMLElement).toBe(true);
   });
 });
+
+describe("printCards", () => {
+  it("should render without crashing", () => {
+    const array = [{imagem: "1", id: "2",}, {imagem: "2", id: "3"}];
+    document.body.innerHTML="<div id='board'></div>";
+    const el = printCards(array);
+    console.log(document.querySelector("#board"));
+    expect(el instanceof HTMLElement).toBe(true);
+  });
+});
+
